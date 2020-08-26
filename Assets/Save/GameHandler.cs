38cc09afeeeff7c0edb638c4eb06281f129697c8
Fredrik Brockert <fredrik@spitfire.se>
2020-08-26 09:27:03 +0200

    Adding first repo

---
+++ Assets/Save/GameHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerGameObject;
+
+    private PlayerToSave unit;
+
+    [SerializeField]
+    public List<Vector3> posPlay = new List<Vector3>();
+
+    private void Awake()
+    {
+        unit = playerGameObject.GetComponent<PlayerToSave>();
+
+        SaveSystem.Init(); //Initiate savesys.
+        
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    private void Save()
+    {
+        Vector3 playerPosition = playerGameObject.transform.position;
+        int goldAmount = unit.goldAmount;
+
+        SaveObject saveObject = new SaveObject
+        {
+            goldAmount = goldAmount,
+            playerPosition = playerPosition,
+            pos = posPlay
+            
+        };
+        string json = JsonUtility.ToJson(saveObject);
+        SaveSystem.Save(json);
+        
+    }
+    private void Load()
+    {
+        string saveString = SaveSystem.Load();
+        if(saveString != null)
+        {
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+            playerGameObject.transform.position = saveObject.playerPosition;
+            unit.goldAmount = saveObject.goldAmount;
+            posPlay = saveObject.pos;
+        }
+  
+        else
+        {
+            Debug.Log("NO Save");
+        }
+    }
+
+
+
+    private class SaveObject{
+        public int goldAmount;
+        public Vector3 playerPosition;
+        public List<Vector3> pos;
+
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/MenuPushButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPushButton : MonoBehaviour
+{
+    public float pressedTimer = 0.0f;
+    public float release;
+    public float clampedTimer;
+    public bool buttonIsPressedDown = false;
+   
+
+    //public int pressedTimerInt;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+       
+    }
+
+    
+    void Update()
+    {
+        if (buttonIsPressedDown == true)
+        {
+            pressedTimer += Time.deltaTime * 3.0f;
+            clampedTimer = Mathf.Clamp(pressedTimer, 0, 1);
+            anim.SetFloat("Blend", clampedTimer);
+        }
+
+        if (buttonIsPressedDown == false)
+        {
+            
+            pressedTimer = 0.0f;
+            anim.SetFloat("Blend", 0);
+        }
+    }
+
+    public void FingerDown()
+    {
+        buttonIsPressedDown = true;
+    }
+
+    public void FingerUp()
+    {
+        buttonIsPressedDown = false;
+    }
+}
